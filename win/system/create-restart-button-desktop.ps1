29b4d76c462b536837f47cf5374d0afce498b919
limelime <limelime@users.noreply.github.com>
2020-06-04 21:42:47 -0400

    u

---
+++ win/system/create-restart-button-desktop.ps1
@@ -0,0 +1,12 @@
+# Description: Create restart.bat on the Desktop of current user.
+
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+
+$restartBatch=
+@"
+echo Press any key to restart.
+pause > nul
+shutdown /r
+"@
+
+$restartBatch | Out-File -FilePath $desktopPath\restart.bat
